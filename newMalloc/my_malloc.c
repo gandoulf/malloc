6729461ae3c98d83ab1fdfc05ba9d7c6267ef77f
agor_m <maxime.agor@epitech.eu>
2016-02-06 12:52:00 +0100

    Fixed memory alignement, malloc of 0 now returns NULL

--- newMalloc/my_malloc.c
+++ newMalloc/my_malloc.c
@@ -5,17 +5,20 @@
 ** Login   <resse_e@epitech.net>
 ** 
 ** Started on  Sat Feb  6 12:20:25 2016 Enzo Resse
-** Last update Sat Feb  6 12:53:15 2016 Enzo Resse
+** Last update Sat Feb  6 12:50:57 2016 Maxime Agor
 */
 
 #include "my_malloc.h"
 
-static void     *start = NULL;
-static void     *end = NULL;
+static void	*start = NULL;
+static void	*end = NULL;
 
 void    *malloc(size_t size)
 {
   void  *ptr;
+
+  if (!size)
+    return NULL;
   if (start == NULL)
     {
       start = sbrk(0);
@@ -28,7 +31,7 @@ void    *malloc(size_t size)
   printf("start = %p, end = %p\n", start, end);
   printf("size of malloc = %d\n", size);
 #endif
-  size += sizeof(int) - (size % sizeof(int));
+  size += (size % sizeof(int)) ? sizeof(int) - (size % sizeof(int)) : 0;
   ptr = findMemory(start, end, size);
 #ifdef DEBUG
   printf("\nafter findMemory ptr = %p\n", ptr);
@@ -36,13 +39,13 @@ void    *malloc(size_t size)
   if (addMemory(start, &end, &ptr, size) == 0)
     return (0);
   /*#ifdef DEBUG
-  printf("\nafter addMemory ptr = %p\n", ptr);
-  printf("after addMemory start = %p, end = %p\n", start, end);
-#endif
-  useMemory(ptr, end, size);
-  ptr += sizeof(t_metadata);
-#ifdef DEBUG
-  printf("\n\n");
-  #endif*/
+    printf("\nafter addMemory ptr = %p\n", ptr);
+    printf("after addMemory start = %p, end = %p\n", start, end);
+    #endif
+    useMemory(ptr, end, size);
+    ptr += sizeof(t_metadata);
+    #ifdef DEBUG
+    printf("\n\n");
+    #endif*/
   return(ptr);
 }
